6d7c8d5f4f03fc19cead76ab761a8e8d9794d5c9
Wesley <wesley100002001@gmail.com>
2020-02-20 15:36:43 +0800

    Initial commit

---
+++ pages/_.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    國會無雙搬新家啦！出發～阿草帶你轉址中～～
+    <img :src="img" height="400"/>
+  </main>
+</template>
+
+<script>
+import articleTable from '~/data/musouMigration.json'
+import defaultImg from 'watchout-common-assets/images/default.gif'
+
+function findMapping (target) {
+  return articleTable.migration.find(item => {
+    return item.musou_id.toString() === target
+  })
+}
+
+export default {
+  beforeMount () {
+    let mapping = null
+    if (/^focuses\/[0-9]+$/.test(this.$route.params.pathMatch)) {
+      const target = this.$route.params.pathMatch.substring(8)
+      mapping = findMapping(target)
+    } else if (/^videos\/[0-9]+$/.test(this.$route.params.pathMatch)) {
+      const target = this.$route.params.pathMatch.substring(7)
+      mapping = findMapping(target)
+    }
+    if (mapping) {
+      window.location = `https://musou.watchout.tw/read/${mapping.fs_id}`
+    } else {
+      window.location = 'https://musou.watchout.tw/'
+    }
+  },
+  data () {
+    return {
+      img: defaultImg
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    國會無雙搬新家啦！出發～阿草帶你轉址中～～
+    <img :src="img" height="400"/>
+  </main>
+</template>
+
+<script>
+import defaultImg from 'watchout-common-assets/images/default.gif'
+
+export default {
+  data () {
+    return {
+      img: defaultImg
+    }
+  },
+  beforeMount () {
+    window.location = 'https://musou.watchout.tw/'
+  }
+}
+</script>
